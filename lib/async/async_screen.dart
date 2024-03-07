8b58d59c0fdd89b9bd72522c83bde5894922fbb2
Pranaya <pranaya.anargya2@homecredit.co.id>
2024-03-07 22:56:43 +0700

    feat: async feature

--- lib/async/async_screen.dart
+++ lib/async/async_screen.dart
@@ -1,4 +1,8 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+
+import 'model/data.dart';
+import 'retrofit/api_client.dart';
 
 class AsynchronousScreen extends StatefulWidget {
   const AsynchronousScreen({super.key});
@@ -9,12 +13,58 @@ class AsynchronousScreen extends StatefulWidget {
 
 class _AsynchronousScreenState extends State<AsynchronousScreen> {
   @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text('ASYNC SCREEN'),
+        title: const Text("Flutter - Retrofit Implementation"),
       ),
-      body: Container(),
+      body: buildBody(context),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {},
+        label: const Icon(Icons.cancel),
+        backgroundColor: Colors.green,
+      ),
+    );
+  }
+
+  FutureBuilder<ResponseData> buildBody(BuildContext context) {
+    final client = ApiClient(Dio(BaseOptions(contentType: "application/json")));
+    return FutureBuilder<ResponseData>(
+      future: client.getUsers(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          final ResponseData posts = snapshot.data!;
+          return buildListView(context, posts);
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+
+  Widget buildListView(BuildContext context, ResponseData posts) {
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return Card(
+          child: ListTile(
+            leading: const Icon(
+              Icons.account_box,
+              color: Colors.green,
+              size: 50,
+            ),
+            title: Text(
+              posts.data[index]['name'],
+              style: const TextStyle(fontSize: 20),
+            ),
+            subtitle: Text(posts.data[index]['email']),
+          ),
+        );
+      },
+      itemCount: posts.data.length,
     );
   }
 }
---
+++ lib/async/model/data.dart
@@ -0,0 +1,36 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'data.g.dart';
+
+@JsonSerializable()
+class User {
+  int id;
+  String name;
+  String email;
+  String gender;
+  String status;
+  String createdAt;
+  String updatedAt;
+
+  User(
+      {required this.id,
+      required this.name,
+      required this.email,
+      required this.gender,
+      required this.status,
+      required this.createdAt,
+      required this.updatedAt});
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+  Map<String, dynamic> toJson() => _$UserToJson(this);
+}
+
+@JsonSerializable()
+class ResponseData {
+  int code;
+  dynamic meta;
+  List<dynamic> data;
+  ResponseData({required this.code, required this.meta, required this.data});
+  factory ResponseData.fromJson(Map<String, dynamic> json) =>
+      _$ResponseDataFromJson(json);
+  Map<String, dynamic> toJson() => _$ResponseDataToJson(this);
+}
