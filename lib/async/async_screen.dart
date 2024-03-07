06b13d1587188c585fa5ecf3f629488a2e29d0dd
Pranaya <pranaya.anargya2@homecredit.co.id>
2024-03-07 18:16:02 +0700

    feat: added components

---
+++ lib/async/async_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AsynchronousScreen extends StatefulWidget {
+  const AsynchronousScreen({super.key});
+
+  @override
+  State<AsynchronousScreen> createState() => _AsynchronousScreenState();
+}
+
+class _AsynchronousScreenState extends State<AsynchronousScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/sembast/sembast_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SembastScreen extends StatefulWidget {
+  const SembastScreen({super.key});
+
+  @override
+  State<SembastScreen> createState() => _SembastScreenState();
+}
+
+class _SembastScreenState extends State<SembastScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
