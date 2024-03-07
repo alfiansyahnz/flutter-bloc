9ffbdcb9d3286b38cf7363bf7c2a8f337424df2e
Pranaya <pranaya.anargya2@homecredit.co.id>
2024-03-07 23:33:28 +0700

    fix: provider fix

--- lib/bloc/bloc_screen.dart
+++ lib/bloc/bloc_screen.dart
@@ -14,37 +14,42 @@ class _BlocScreenState extends State<BlocScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Demo Test'),
+        title: const Text('Demo Test'),
       ),
-      body: BlocProvider(
-        create: (_) => DemoBloc(),
-        child: BlocBuilder<DemoBloc, DemoState>(
-          builder: (context, state) {
-            return Center(
+      body: BlocProvider(create: (_) => DemoBloc(), child: _content(context)),
+    );
+  }
+
+  Widget _content(BuildContext context) {
+    return BlocBuilder<DemoBloc, DemoState>(
+      builder: (context, state) {
+        return Column(
+          children: [
+            Center(
               child: Text('${state.number}'),
-            );
-          },
-        ),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: <Widget>[
-          FloatingActionButton(
-            child: const Icon(Icons.add),
-            onPressed: () =>
-                context.read<DemoBloc>().add(const DemoEvent.increment()),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          FloatingActionButton(
-            child: const Icon(Icons.remove),
-            onPressed: () =>
-                context.read<DemoBloc>().add(const DemoEvent.decrement()),
-          ),
-        ],
-      ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                  child: const Icon(Icons.add),
+                  onPressed: () =>
+                      context.read<DemoBloc>().add(const DemoEvent.increment()),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextButton(
+                  child: const Icon(Icons.remove),
+                  onPressed: () =>
+                      context.read<DemoBloc>().add(const DemoEvent.decrement()),
+                ),
+              ],
+            ),
+          ],
+        );
+      },
     );
   }
 }
